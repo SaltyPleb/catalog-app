b67f1fcde456b432119f06238752ad34d3f56f57
SaltyPleb <sergun4ik22@gmail.com>
2021-09-12 17:25:05 +0300

    Change logo

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React from "react";
 import {
   Nav,
   NavLink,
   Bars,
   NavMenu,
   NavBtn,
-  NavBtnLink
-} from './NavbarElements.js';
+  NavBtnLink,
+} from "./NavbarElements.js";
 
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavLink to="/">
-          <img src="https://www.freepnglogos.com/uploads/2021-png/hd-free-2021-transparent-new-year-png-images-0.png" width="50" alt="hd free 2021 transparent new year png images" />
+          <img
+            src="https://www.freepnglogos.com/uploads/circle-png/up-arrow-circle-logo-png-transparent-33.png"
+            width="40"
+            alt="img"
+          />
           <h1 className="nav-name">Salty Salty app</h1>
         </NavLink>
         <Bars />
