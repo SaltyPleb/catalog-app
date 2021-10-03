7672504079a215e3d1e46330a9cdfc073f3df756
SaltyPleb <sergun4ik22@gmail.com>
2021-10-03 19:17:58 +0300

    hmm need second useState hook

--- src/pages/catalog/types.js
+++ src/pages/catalog/types.js
@@ -4,23 +4,67 @@ import { Context } from "../..";
 import "./styles/types.css";
 
 const types = observer(() => {
-  const { device } = useContext(Context)
-  const [checked, setChacked] = useState(new Array(device.brands.length).fill(false));
-  console.log(device.brands.length)
-  const handlerChecked = (position) =>{
-    const updatedCheckedState = checked.map((item, id) => id === position ? !item : item)
-    setChacked(updatedCheckedState)
-  }
+  const { device } = useContext(Context);
+  const [checked, setChacked] = useState(
+    new Array(device.brands.length).fill(false)
+  );
+
+  const [devicechecked, setDeviceChacked] = useState(
+    new Array(device.devices.length).fill(false)
+  );
+  
+    console.log(devicechecked);
+  const handlerDeviceChecked = (position) => {
+    const updatedDeviceCheckedState = checked.map((item, id) =>
+      id === position ? !item : item
+    );
+    setDeviceChacked(updatedDeviceCheckedState);
+  };
+
+  const handlerChecked = (position) => {
+    const updatedCheckedState = checked.map((item, id) =>
+      id === position ? !item : item
+    );
+    setChacked(updatedCheckedState);
+  };
+
+  const fillDevice = (position) => {
+    return device.devices.map(({ id, name }) =>
+      position === id ? (
+        <div className="brand_holder">
+          <input
+            className="device_checkbox"
+            id={id}
+            type="checkbox"
+            checked={devicechecked[position]}
+            onChange={() => handlerDeviceChecked(position)}
+          />
+          {name}
+        </div>
+      ) : null
+    );
+  };
 
   return (
-    <div>
-      {device.brands.map(({id, name}) => (
-        <label className="label_holder" key={id}>
-            <input type="checkbox" checked={checked[id]} onChange={() => handlerChecked(id)}/>
-            <span className="checkmark"/>
+    <div className="items">
+      {device.brands.map(({ id, name }) => (
+        <>
+          <label className="label_holder" key={id}>
+            <input
+              type="checkbox"
+              checked={checked[id]}
+              onChange={() => handlerChecked(id)}
+            />
+            <span className="checkmark" />
             {name}
-            <img className="img_holder" src="https://image.flaticon.com/icons/png/512/25/25623.png" alt="" />
-        </label>
+            <img
+              className="img_holder"
+              src="https://image.flaticon.com/icons/png/512/25/25623.png"
+              alt=""
+            />
+          </label>
+          <div className="brands_list">{fillDevice(id)}</div>
+        </>
       ))}
     </div>
   );
