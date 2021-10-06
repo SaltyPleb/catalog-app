79b4104d8446081f4804c2fa59f21e5d31ffea85
Yauheni Bulyha <sergun4ik22@gmail.com>
2021-10-06 14:48:29 +0300

    add right panel

--- src/pages/catalog/catalog.js
+++ src/pages/catalog/catalog.js
@@ -15,7 +15,7 @@ class Catalog extends Component {
     value: '',
   };
 
-  
+
   async componentDidMount() {
     const response = await fetch(
       "http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
@@ -30,11 +30,11 @@ class Catalog extends Component {
     });
   }
   onChange = (event) => {
-    this.setState({value: event.target.value})
+    this.setState({ value: event.target.value })
   }
 
   onClear = () => {
-    this.setState({value: ""})
+    this.setState({ value: "" })
   }
 
   // onChange = () => {
@@ -49,43 +49,49 @@ class Catalog extends Component {
         {this.state.isloading ? (
           <Loader />
         ) : (
-          <div className="main_grid">
-            <div className="search__grid">
-              <div className="search__bar">
-                <div className="search__bar-wrapper">
-                  <div className="closeModal" onClick={this.onClear}></div>
-                  <div className="search__bar-suggest">
-                    <input
-                      className="search__input"
-                      type="text"
-                      placeholder="Поиск"
-                      value={this.state.value} 
-                      onChange={this.onChange}
-                    />
-                  </div>
-                </div>
-
-                {/* <a href="#" class="close" /> */}
-              </div>
-            </div>
-            <div className="middle_grid">
-              {" "}
-              <div className="grid_leftcolumn">
-                <div className="filter_wrapper">
-                  <div className="sheema_filter">
-                    <Types/>
-                  </div>
-                </div>
-              </div>
-              <div className="right_grid">
-                <div className="grid">
-                  {/* {this.state.data.map(() => ( */}
-                    <Card />
-                  {/* ))} */}
-                </div>
-              </div>
-            </div>
+          <> <div className="right_bar">
+          <div className="right_btn text__shadow">Likes</div>
+          <div className="right_btn text__shadow">History</div>
           </div>
+            <div className="main_grid">
+              <div className="search__grid">
+                <div className="search__bar">
+                  <div className="search__bar-wrapper">
+                    <div className="closeModal" onClick={this.onClear}></div>
+                    <div className="search__bar-suggest">
+                      <input
+                        className="search__input"
+                        type="text"
+                        placeholder="Поиск"
+                        value={this.state.value}
+                        onChange={this.onChange}
+                      />
+                    </div>
+                  </div>
+
+                  {/* <a href="#" class="close" /> */}
+                </div>
+              </div>
+              <div className="middle_grid">
+                {" "}
+                <div className="grid_leftcolumn">
+                  <div className="filter_wrapper">
+                    <div className="sheema_filter">
+                      <Types />
+                    </div>
+                  </div>
+                </div>
+                <div className="right_grid">
+                  <div className="grid">
+                    {/* {this.state.data.map(() => ( */}
+                    <Card />
+                    {/* ))} */}
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </>
         )}
       </>
     );
