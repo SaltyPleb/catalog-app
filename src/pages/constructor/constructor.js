c43c939153b4c30193d9c329d0f493fa572e1916
SaltyPleb <sergun4ik22@gmail.com>
2021-10-14 20:37:16 +0300

    cons

--- src/pages/constructor/constructor.js
+++ src/pages/constructor/constructor.js
@@ -1,19 +1,39 @@
 import React from "react";
 import Rightnav from "../catalog/rightnav";
+import "./constructor.css";
 
 const Constructor = () => {
   return (
     <>
-    <Rightnav/>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "90vh",
-        }}
-      >
-        <h1>Constructor</h1>
+      <div className="constructor_main">
+        <Rightnav />
+        <div className="main">
+          <div className="component_bloks">
+            <div className="plus_block">
+              <div className="big_plus" />
+            </div>
+            <div className="plus_block">
+              2
+              <div className="big_plus" />
+            </div>
+            <div className="plus_block">
+              3
+              <div className="big_plus" />
+            </div>
+            <div className="plus_block">
+              4
+              <div className="big_plus" />
+            </div>
+            <div className="plus_block">
+              5
+              <div className="big_plus" />
+            </div>
+            <div className="plus_block">
+              6
+              <div className="big_plus" />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
