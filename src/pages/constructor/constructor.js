c348ef30e32f1cccc818d787074371651e604e13
SaltyPleb <sergun4ik22@gmail.com>
2021-10-17 15:32:22 +0300

    search result cards table

--- src/pages/constructor/constructor.js
+++ src/pages/constructor/constructor.js
@@ -32,6 +32,80 @@ const Constructor = () => {
               6
               <div className="big_plus" />
             </div>
+            <div className="plus_block">
+              7
+              <div className="big_plus" />
+            </div>
+            <div className="plus_block">
+              8
+              <div className="big_plus" />
+            </div>
+          </div>
+          <div className="search_results">
+            {/* draft */}
+            <div className="res_cards">
+              <div className="serach_card">
+                <img className="search_img" src="https://www.asus.com/media/global/gallery/797CKaxpZhF41vQA_setting_xxx_0_90_end_800.png" alt="loading" />
+                <div className="res_info">asudhua</div>
+                <div className="link_icon"/>
+              </div>
+              <div className="serach_card">
+                <img className="search_img" src="https://www.asus.com/media/global/gallery/797CKaxpZhF41vQA_setting_xxx_0_90_end_800.png" alt="loading" />
+                <div className="res_info">asudhua</div>
+                <div className="link_icon"/>
+              </div>
+              <div className="serach_card">
+                <img className="search_img" src="https://www.asus.com/media/global/gallery/797CKaxpZhF41vQA_setting_xxx_0_90_end_800.png" alt="loading" />
+                <div className="res_info">asudhua</div>
+                <div className="link_icon"/>
+              </div>
+              <div className="serach_card">
+                <img className="search_img" src="https://www.asus.com/media/global/gallery/797CKaxpZhF41vQA_setting_xxx_0_90_end_800.png" alt="loading" />
+                <div className="res_info">asudhua</div>
+                <div className="link_icon"/>
+              </div>
+              <div className="serach_card">
+                <img className="search_img" src="https://www.asus.com/media/global/gallery/797CKaxpZhF41vQA_setting_xxx_0_90_end_800.png" alt="loading" />
+                <div className="res_info">asudhua</div>
+                <div className="link_icon"/>
+              </div>
+              <div className="serach_card">
+                <img className="search_img" src="https://www.asus.com/media/global/gallery/797CKaxpZhF41vQA_setting_xxx_0_90_end_800.png" alt="loading" />
+                <div className="res_info">asudhua</div>
+                <div className="link_icon"/>
+              </div>
+              <div className="serach_card">
+                <img className="search_img" src="https://www.asus.com/media/global/gallery/797CKaxpZhF41vQA_setting_xxx_0_90_end_800.png" alt="loading" />
+                <div className="res_info">asudhua</div>
+                <div className="link_icon"/>
+              </div>
+              <div className="serach_card">
+                <img className="search_img" src="https://www.asus.com/media/global/gallery/797CKaxpZhF41vQA_setting_xxx_0_90_end_800.png" alt="loading" />
+                <div className="res_info">asudhua</div>
+                <div className="link_icon"/>
+              </div>
+              <div className="serach_card">
+                <img className="search_img" src="https://www.asus.com/media/global/gallery/797CKaxpZhF41vQA_setting_xxx_0_90_end_800.png" alt="loading" />
+                <div className="res_info">asudhua</div>
+                <div className="link_icon"/>
+              </div>
+              <div className="serach_card">
+                <img className="search_img" src="https://www.asus.com/media/global/gallery/797CKaxpZhF41vQA_setting_xxx_0_90_end_800.png" alt="loading" />
+                <div className="res_info">asudhua</div>
+                <div className="link_icon"/>
+              </div>
+              <div className="serach_card">
+                <img className="search_img" src="https://www.asus.com/media/global/gallery/797CKaxpZhF41vQA_setting_xxx_0_90_end_800.png" alt="loading" />
+                <div className="res_info">asudhua</div>
+                <div className="link_icon"/>
+              </div>
+              <div className="serach_card">
+                <img className="search_img" src="https://www.asus.com/media/global/gallery/797CKaxpZhF41vQA_setting_xxx_0_90_end_800.png" alt="loading" />
+                <div className="res_info">asudhua</div>
+                <div className="link_icon"/>
+              </div>
+              
+            </div>
           </div>
         </div>
       </div>
