d3a003799c79cc013d83a0dc633fbf4323c02a6e
SaltyPleb <sergun4ik22@gmail.com>
2021-10-31 17:51:29 +0300

    new app import

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router , Switch, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navbar';
 import AppRouter from './components/routes/AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
